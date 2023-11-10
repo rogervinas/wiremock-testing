cfb5bd40990a5a184ece6e2bd31ccabb4e04701c
Roger Viñas Alcon <roger.vinas@fastzink.com>
2023-11-10 17:31:02 +0100

    Enable gradle scan (#29)
    * Enable gradle scan
    
    * Fix job name
    
    * Enable gradle scan on CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
 rootProject.name = "wiremock-testing"
+
+plugins {
+  id("com.gradle.enterprise") version("3.15.1")
+}
+
+gradleEnterprise {
+  buildScan {
+    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+    termsOfServiceAgree = "yes"
+  }
+}
